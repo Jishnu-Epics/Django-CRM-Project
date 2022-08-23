633f6d3ee8702cca3d0d6919db57c607fee33cc0
Jishnu-Epics <jishnuvv2016@gmail.com>
2022-08-23 21:30:49 +0530

    feat

--- leads/urls.py
+++ leads/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . views import  lead_create, lead_update, leads_list,lead_details    
+from . views import  lead_create, lead_delete, lead_update, lead_list,lead_detail   
 
 app_name = "leads"
 
 urlpatterns = [
     
-    path('',leads_list),
-    path('<int:pk>/',lead_details),
-    path('<int:pk>/update/',lead_update),
-    path('create',lead_create),
+    path('',lead_list, name='lead-list'),
+    path('<int:pk>/', lead_detail, name='lead-detail'),
+    path('<int:pk>/update/',lead_update, name='lead-update'),
+    path('<int:pk>/delete/',lead_delete,name='lead-delete'),
+    path('create/',lead_create, name='lead-create'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,13 +1,9 @@
-from calendar import leapdays
-from multiprocessing import context
-from unicodedata import name
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from . models import Lead,Agent
-from . forms import LeadForm,LeadModelForm
+from . models import Lead
+from . forms import LeadModelForm
 
 
-def leads_list(request):
+def lead_list(request):
     leads = Lead.objects.all( )
     context = {
         "leads":leads,
@@ -15,7 +11,7 @@ def leads_list(request):
     }
     return render(request, "leads/lead_list.html",context)
 
-def lead_details(request,pk):
+def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
     context = {
         "lead":lead
@@ -36,27 +32,53 @@ def lead_create(request):
         }
     return render(request,"leads/lead_create.html",context)
 
-def lead_update(request,pk):
-     lead = Lead.objects.get(id=pk)
-     form = LeadForm()
-     if request.method == "POST":
-        form = LeadForm(request.POST)
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            lead.first_name = first_name
-            lead.last_name = last_name
-            lead.age = age
-            lead.save()
+            form.save()
     
             return redirect("/leads")
-     context = {
+    context = {
         "forms":LeadModelForm(),
         "lead": lead
          }
     
-     return render(request, "leads/lead_update.html",context)
+    return render(request, "leads/lead_update.html",context)  
+
+
+
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")         
+
+
+# def lead_update(request,pk):
+#      lead = Lead.objects.get(id=pk)
+#      form = LeadForm()
+#      if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+#             lead.save()
+    
+#             return redirect("/leads")
+    #  context = {
+    #     "forms":LeadModelForm(),
+    #     "lead": lead
+    #      }
+    
+    #  return render(request, "leads/lead_update.html",context)
 
 
 
