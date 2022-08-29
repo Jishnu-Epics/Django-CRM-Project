646c8a2a7714e0a5970340af4892610ce2b7f590
Jishnu-Epics <jishnuvv2016@gmail.com>
2022-08-29 13:31:28 +0530

    feat:changes

--- leads/models.py
+++ leads/models.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    is_organisor = models.BooleanField(default=True)
+    is_agent = models.BooleanField(default=False)
 
 
 class UserProfile(models.Model):
